538e087b0e49011037ffd4624873a2b20bf49163
Sara Meza <sara.meza@uac.edu.co>
2025-12-03 23:16:47 -0500

    Eliminacion clases no usadas

--- app/src/main/java/com/example/marvel_comic_app/ui/adapters/SpinnerAdapter.java
+++
@@ -1,4 +0,0 @@
-package com.example.marvel_comic_app.ui.adapters;
-
-public class SpinnerAdapter {
-}
--- app/src/main/java/com/example/marvel_comic_app/ui/fragments/SolicitarFragment.java
+++
@@ -1,4 +0,0 @@
-package com.example.marvel_comic_app.ui.fragments;
-
-public class SolicitarFragment {
-}
